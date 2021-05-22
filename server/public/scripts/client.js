5f5fd5ae79e4fcd33a78008868ab22b283a6c1b3
Owen Carruth <ocarr199@gmail.com>
2021-05-22 15:46:07 -0500

    moved over to server, not working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,31 +2,16 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!");
+getCalc()
+//   click listeners
 $("#equals").on('click', Calculate)
 $("#add").on('click', clickedAdd)
 $("#subtract").on('click', clickedSubtract)
 $("#multiply").on('click', clickedMultiply)
 $("#divide").on('click', clickedDivide)
+$("#clear").on('click', clearInp)
 }
 
-let operator;
-
-function Calculate (){
-    let numberOne = Number($('#num1').val());
-let numberTwo = Number($('#num2').val());
-
-let answer;
-if(operator == "add"){
-    answer = numberOne + numberTwo;
-} else if(operator == "subtract"){
-    answer = numberOne - numberTwo;
-}  else if(operator == "multiply"){
-    answer = numberOne * numberTwo;
-}  else if(operator == "divide"){
-    answer = numberOne / numberTwo;
-} 
-console.log(answer);
-}
 
 function clickedAdd (){
     operator = "add"
@@ -47,3 +32,45 @@ function clickedDivide (){
     console.log(operator);
 
 }
+function clearInp (){
+    $('#num1').val('')
+    $('#num1').val('')
+}
+function getCalc(){
+
+$.ajax({
+    method: "GET",
+    url: "/calc",
+  }).then(function (response) {
+    // response is what was in the res.send()
+    console.log(response);
+
+  });
+}
+
+  let operator;
+  let numberOne = Number($('#num1').val());
+  let numberTwo = Number($('#num2').val());
+
+  function Calculate (){
+let newCalc = {
+    numberOne:   Number($('#num1').val()),
+    numberTwo:    Number($('#num2').val()),
+    operator: operator,
+    answer
+}
+
+    $.ajax({
+        method: "POST",
+        url: "/calc",
+        data: newCalc,
+      }).then(function (response) {
+        // response is what was in the res.send()
+        console.log(response);
+getCalc()
+       
+      });
+  }
+ 
+
+
--- server/server.js
+++ server/server.js
@@ -8,17 +8,49 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static("server/public"));
+// #region logic
+
+
 
 let numbers = ["hello"]
+
+function doTheMath(num1,num2,operator)  {
+let answer;
+if(operator == +){
+    answer = num1 + num2;
+} else if(operator == "subtract"){
+    answer = num1 - num2;
+}  else if(operator == "multiply"){
+    answer = num1 * num2;
+}  else if(operator == "divide"){
+    answer = num1 / num2;
+} 
+console.log(answer);
+}
+
+// //#endregion logic
 
 // #region get and post
 app.get("/calc", (req, res) => {
     console.log("got to /guesses");
-  
+
     // respond
     res.send(numbers);
   });
 
+  app.post("/calc", (req, res) => {
+    // We want to add the incoming data to /calc
+    // req.body is made by body-parser
+    // info from client
+    let dataRecieved = req.body
+    console.log(req.body);
+    numbers.push(req.body);
+    console.log(dataRecieved.numberOne)
+    // send back a good response
+    res.send(req.body)
+    
+    res.sendStatus(201);
+  });
 
 // //#endregion get and post 
 

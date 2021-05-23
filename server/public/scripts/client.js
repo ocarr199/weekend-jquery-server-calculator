4c76630d5daa3df8f8d3e574195fdead84d2532e
Owen Carruth <ocarr199@gmail.com>
2021-05-22 20:24:41 -0500

    code formatted, removed useless code, commented out

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,82 +2,87 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!");
-getCalc()
-//   click listeners
-$("#equals").on('click', Calculate)
-$("#add").on('click', clickedAdd)
-$("#subtract").on('click', clickedSubtract)
-$("#multiply").on('click', clickedMultiply)
-$("#divide").on('click', clickedDivide)
-$("#clear").on('click', clearInp)
+  getCalc()
+  //   click listeners
+  $("#equals").on('click', Calculate)
+  $("#add").on('click', clickedAdd)
+  $("#subtract").on('click', clickedSubtract)
+  $("#multiply").on('click', clickedMultiply)
+  $("#divide").on('click', clickedDivide)
+  $("#clear").on('click', clearInp)
 }
 
-
-function clickedAdd (){
-    operator = "+"
-    console.log(operator);
+// functions to decide what operator to use
+function clickedAdd() {
+  operator = "+"
+  console.log(operator);
 }
 
-function clickedSubtract (){
-    operator = "-"
-    console.log(operator);
+function clickedSubtract() {
+  operator = "-"
+  console.log(operator);
+}
 
+function clickedMultiply() {
+  operator = "*"
+  console.log(operator);
 }
-function clickedMultiply (){
-    operator = "*"
-    console.log(operator);
-}
-function clickedDivide (){
-    operator = "/"
-    console.log(operator);
 
+function clickedDivide() {
+  operator = "/"
+  console.log(operator);
 }
-function clearInp (){
-    $('#num1').val('')
-    $('#num2').val('')
+// clear text inputs
+function clearInp() {
+  $('#num1').val('')
+  $('#num2').val('')
 }
-function getCalc(){
-
-$.ajax({
+// function run on page load
+// retrieves numbers array
+function getCalc() {
+  $.ajax({
+    // finding app.get('/calc' in sever.js)
     method: "GET",
     url: "/calc",
+    // what happens once app.get is found
+    // response is what is sent from the get
+    // in this case response is the numbers array
   }).then(function (response) {
     // response is what was in the res.send()
     console.log(response);
     $('#mathProblems').empty()
-    for(let answer of response){
-        $('#lastAnswer').text(`${answer.answer}`)
-        $('#mathProblems').prepend(`
+    for (let answer of response) {
+      $('#lastAnswer').text(`${answer.answer}`)
+      $('#mathProblems').prepend(`
         <li>${answer.numberOne} ${answer.operator} ${answer.numberTwo} = ${answer.answer}</li>
         `)
     }
   });
 }
-
-  let operator;
-  let numberOne = Number($('#num1').val());
-  let numberTwo = Number($('#num2').val());
-
-  function Calculate (){
-let newCalc = {
-    numberOne:   Number($('#num1').val()),
-    numberTwo:    Number($('#num2').val()),
+// declaring operator variable
+let operator;
+// function run when equals button is clicked
+function Calculate() {
+  // declaring objects that will be put in numbers array
+  let newCalc = {
+    // first number
+    numberOne: Number($('#num1').val()),
+    // second number
+    numberTwo: Number($('#num2').val()),
+    // operator
     operator: operator,
-}
-
-    $.ajax({
-        method: "POST",
-        url: "/calc",
-        data: newCalc,
-      }).then(function (response) {
-        // response is what was in the res.send()
-        console.log(response);
-      
-       
-getCalc()
-       
-      });
   }
- 
-
-
+  // ajax finds app.post('/calc' and sends it 
+  // the newCalc object
+  $.ajax({
+    method: "POST",
+    url: "/calc",
+    data: newCalc,
+  }).then(function (response) {
+    // response is status code
+    // this time it will log "created" bc code 201
+    console.log(response);
+    // run getCalc again with updated numbers array
+    getCalc()
+  });
+}
--- server/server.js
+++ server/server.js
@@ -4,79 +4,69 @@ const app = express();
 const PORT = 5000;
 
 // This must be added before GET & POST routes.
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static("server/public"));
 // #region logic
 
 
-
+// declaring array to push our newCalc objects to
 let numbers = []
-
-function doTheMath(list)  {
-
-// for(let obj of list){
-//     console.log(obj)
-//     if(obj.operator == '+'){
-//     obj.answer = obj.numberOne + obj.numberTwo;
-//  }else{
-//      console.log("doTheMath broke")
-//  }
-// //   else if(list.operator == "-"){
-// //      answer = num1 - num2;
-// //  }  else if(operator == "multiply"){
-// //      answer = num1 * num2;
-// //  }  else if(operator == "divide"){
-// //      answer = num1 / num2;
-// //  } 
-// }
-
-// console.log(answer);
-for(let obj of list){
-    if(obj.operator == "+"){
-        obj.answer = Number(obj.numberOne) + Number(obj.numberTwo);
-        console.log(obj.answer)
-    } else if(obj.operator == "-"){
-        obj.answer = Number(obj.numberOne) - Number(obj.numberTwo);
-        console.log(obj.answer)
-    } else if(obj.operator == "*"){
-        obj.answer = Number(obj.numberOne) * Number(obj.numberTwo);
-        console.log(obj.answer)
-    } else if(obj.operator == "/"){
-        obj.answer = Number(obj.numberOne) / Number(obj.numberTwo);
-        console.log(obj.answer)
+// function to decide what operator to use on the 2 number inputs
+function doTheMath(list) {
+    // for loop to go through numbers array
+    for (let obj of list) {
+        // each conditional looks at operator clicked on
+        // from the object.operator in the number array
+        // operator clicked on decides what math expression to run
+        if (obj.operator == "+") {
+            obj.answer = Number(obj.numberOne) + Number(obj.numberTwo);
+            console.log(obj.answer)
+        } else if (obj.operator == "-") {
+            // adding .answer property to obj
+            // obj is each object in numbers array
+            // those object are the newCalc object 
+            obj.answer = Number(obj.numberOne) - Number(obj.numberTwo);
+            console.log(obj.answer)
+        } else if (obj.operator == "*") {
+            obj.answer = Number(obj.numberOne) * Number(obj.numberTwo);
+            console.log(obj.answer)
+        } else if (obj.operator == "/") {
+            obj.answer = Number(obj.numberOne) / Number(obj.numberTwo);
+            console.log(obj.answer)
+        } else {
+            console.log('You broke it dummy o_0')
+        }
     }
-  
 }
-}
-
 // //#endregion logic
 
 // #region get and post
 app.get("/calc", (req, res) => {
     console.log("got to /calc");
+    // doTheMath on the array numbers
+    // this calculates the final answer of the 
+    // numbers and operator
     doTheMath(numbers)
-    // respond
+    // send the numbers array as a response
+    // now that the function doTheMath has been run on it
     res.send(numbers);
-  });
+});
 
-  app.post("/calc", (req, res) => {
-    // We want to add the incoming data to /calc
-    // req.body is made by body-parser
-    // info from client
-    let dataRecieved = req.body
+app.post("/calc", (req, res) => {
+    //   req.body is data from ajax post
     console.log(req.body);
+    // pushing data (newCalc Object) into numbers
     numbers.push(req.body);
-
     // send back a good response
-    res.send(req.body)
-    
     res.sendStatus(201);
-  });
+});
 
 // //#endregion get and post 
 
 app.listen(PORT, () => {
     console.log("Server is running on port", PORT);
-  });
+});
